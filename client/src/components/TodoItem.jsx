111ccd7d5d01ddae314135845aaead841f49a10b
abhinav <abhinavisgreat18@gmail.com>
2025-01-30 22:23:45 +0530

    onclicking task dialog will open

--- client/src/components/TodoItem.jsx
+++ client/src/components/TodoItem.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 
 const TodoItem = ({data,checkBox}) => {
+
+  const handleDialog=()=>{
+    
+  }
   return (
     <div className='flex items-center gap-2'>
       <Checkbox
         checked={data.checked}
         onCheckedChange={checkBox}
       />
-      <p className={data.checked ? 'line-through' : ''}>{data.task}</p>
+      <Dialog>
+        <DialogTrigger>
+          <p onClick={handleDialog} className={data.checked ? 'line-through cursor-pointer hover:underline' : 'cursor-pointer hover:underline'}>{data.task}</p>
+        </DialogTrigger>
+        <DialogContent>
+          <div>
+            
+          </div>
+        </DialogContent>
+      </Dialog>
+
     </div>
   )
 }
