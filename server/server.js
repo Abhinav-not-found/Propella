b0a8d115d0c878c3e8b7a60d6db86f4f3b5d65ec
abhinav <abhinavisgreat18@gmail.com>
2025-02-08 21:07:30 +0530

    updated cors 2

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(morgan('dev'))
 app.use(cors({
   origin: ['https://propella-psi.vercel.app/', 'http://localhost:5173/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials:true,
 }))
 app.use(cookieParser())
 
