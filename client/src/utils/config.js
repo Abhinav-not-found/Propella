a6b5de3079939d1656de153fe1eba4f2fcd01750
abhinav <abhinavisgreat18@gmail.com>
2025-02-08 20:41:06 +0530

    updated

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,2 +1,5 @@
-const backendUri = import.meta.env.VITE_BACKEND_URI_DEV || import.meta.env.VITE_BACKEND_URI_PROD
+const backendUri = import.meta.env.MODE === 'development' 
+  ? import.meta.env.VITE_BACKEND_URI_DEV 
+  : 'https://propella-backend.vercel.app';
+
 export default backendUri;
